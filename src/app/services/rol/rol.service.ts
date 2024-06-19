81ebc9812e93d7019981935af79b807232e8c7dc
Sammy593 <samuelto1359@gmail.com>
2024-06-19 10:14:28 -0500

    users and roles services

--- src/app/services/rol/rol.service.ts
+++ src/app/services/rol/rol.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,72 @@ import { Injectable } from '@angular/core';
 })
 export class RolService {
 
-  constructor() { }
+  url = "http://localhost:8080/rol/";
+
+  constructor(public http: HttpClient) { }
+
+  save(data: any) {
+    return new Promise(resolve => {
+      this.http.post(this.url + 'save', data).subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  listRoles(){
+    return new Promise(resolve => {
+      this.http.get(this.url + 'listRoles').subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  findById(id: string){
+    return new Promise(resolve => {
+      this.http.get(this.url + 'findById/' + id).subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  deleteById(id: string){
+    return new Promise(resolve => {
+      this.http.delete(this.url + 'deleteById/' + id).subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  updateById(id: string, data: any){
+    return new Promise(resolve => {
+      this.http.put(this.url + 'updateById/' + id, data).subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,72 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  constructor() { }
+  url = "http://localhost:8080/user/";
+
+  constructor(public http: HttpClient) { }
+
+  save(data: any) {
+    return new Promise(resolve => {
+      this.http.post(this.url + 'save', data).subscribe({
+        next: (data) => {
+          resolve(data);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  listUsers(){
+    return new Promise(resolve => {
+      this.http.get(this.url + 'listUsers').subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  findById(id: string){
+    return new Promise(resolve => {
+      this.http.get(this.url + 'findById/' + id).subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  deleteById(id: string){
+    return new Promise(resolve => {
+      this.http.delete(this.url + 'deleteById/' + id).subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
+
+  updateById(id: string, data: any){
+    return new Promise(resolve => {
+      this.http.put(this.url + 'updateById/' + id, data).subscribe({
+        next:(data) =>{
+          resolve(data);
+        },
+        error: (err)=>{
+          console.log(err);
+        }
+      });
+    })
+  }
 }
