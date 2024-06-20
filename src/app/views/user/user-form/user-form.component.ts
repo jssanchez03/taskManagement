d3bb5c9522f1458d93503d72ff99baed62b941c3
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-20 00:31:48 -0500

    cambios en formularios de task, projects and user

--- src/app/views/user/user-form/user-form.component.ts
+++ src/app/views/user/user-form/user-form.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
-export class UserFormComponent {
+export class UserFormComponent implements  OnInit {
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      nombres: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(\s[a-zA-Z]+)*$/)]],
+      apellidos: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(\s[a-zA-Z]+)*$/)]],
+      telefono: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+((gmail\.com)|(espe\.edu\.ec))$/)]],
+      idRol: ['', Validators.required],
+      estado: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      console.log('Formulario válido', this.userForm.value);
+    } else {
+      console.log('Formulario inválido. Revise los campos.');
+    }
+  }
 
 }
