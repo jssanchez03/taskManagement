288f831ec5463f3df58500b04d17b2e4e3379c27
Jair Sánchez <jssanchez9@espe.edu.ec>
2024-06-20 14:45:56 -0500

    Barra de busqueda y tablas

--- src/app/views/projects/project-list/project-list.component.ts
+++ src/app/views/projects/project-list/project-list.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+
 interface Project {
   name: string;
   status: string;
@@ -12,17 +14,28 @@ interface Project {
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [CommonModule, SidebarComponent,  TableModule,
-    ButtonModule,],
+  imports: [
+    CommonModule,
+    SidebarComponent,
+    TableModule,
+    FormsModule,
+    ButtonModule,
+  ],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.css'
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
+  searchTerm: string = '';
+  filteredProjects: Project[] = [];
   projects: Project[] = [
     { name: 'Proyecto 1', status: 'En progreso', progress: 45 },
     { name: 'Proyecto 2', status: 'Completado', progress: 100 },
     { name: 'Proyecto 3', status: 'En espera', progress: 20 }
   ];
+
+  ngOnInit() {
+    this.filteredProjects = [...this.projects];
+  }
 
   editProject(project: Project) {
     console.log('Editar proyecto:', project);
@@ -31,7 +44,22 @@ export class ProjectListComponent {
 
   deleteProject(project: Project) {
     console.log('Eliminar proyecto:', project);
-    // Implementar lógica de eliminación
     this.projects = this.projects.filter(p => p !== project);
+    this.filteredProjects = this.filteredProjects.filter(p => p !== project);
+  }
+
+  viewProject(project: Project) {
+    // Implementa la lógica para ver los detalles del proyecto
+    console.log('Ver proyecto:', project);
+  }
+
+  searchProjects() {
+    if (this.searchTerm) {
+      this.filteredProjects = this.projects.filter(project =>
+        project.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.filteredProjects = [...this.projects];
+    }
   }
 }
